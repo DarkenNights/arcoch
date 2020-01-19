3748015edcce158343b69edd5907aad347991b1c
Thomas Lenormand <thomas_lenormand@hotmail.fr>
2020-01-19 16:59:44 +0100

    Suppression des routes pour s'enregistrer

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ Route::get('/conciergerie', 'ConciergeController@concierge')->name('concierge');
 
 Route::get('/contact', 'ContactController@contact')->name('contact');
 
-Auth::routes();
+//Auth::routes();
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 //Pour se log uniquement mettre en commentaire la ligne Auth::routes() et décommenter celles ci-dessous
-//Route::get('admin/login', 'Auth\LoginController@showLoginForm')->name('login');
-//Route::post('admin/login', 'Auth\LoginController@login');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
 
 Route::get('/nimda/stocks', 'NimdaController@stock')->name('nimdaStock');
 Route::post('/nimda/saveNewStocks', 'NimdaController@saveStocks')->name('nimdaSaveStocks');
