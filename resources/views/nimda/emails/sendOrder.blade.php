64289de0cf3d61ff8e48b7b8057321d98c7d2b58
Thomas Lenormand <thomas_lenormand@hotmail.fr>
2020-01-25 14:20:11 +0100

    MAJ email d'envoie de commande

--- resources/views/nimda/emails/sendOrder.blade.php
+++ resources/views/nimda/emails/sendOrder.blade.php
@@ -7,12 +7,12 @@ Voici la commande que nous souhaiterions passer pour la prochaine livraison :
         <li>{{ $product->quantity }} x {{ $product->product->name }} @if(!empty($product->product->reference)) ( {{ $product->product->reference }} ) @endif</li>
     @endforeach
 </ul>
-<br><br>
+
 Merci d'avance pour vos actions.
 <br><br>
 Cordialement,<br>
-L'équipe de l'Arcoch - SAS Le Petit Canada
-Tel: 09 54 11 54 16
-Addresse : Route du lac de Payolle, 65710 Campan
-<img src="{{ asset('index/img/logo_menu.png') }}">
+L'équipe de l'Arcoch - SAS Le Petit Canada <br>
+Tel: 09 54 11 54 16 <br>
+Addresse : Route du lac de Payolle, 65710 Campan<br>
+<img src="{{ asset('index/img/logo_menu.png') }}" width="800px">
 
